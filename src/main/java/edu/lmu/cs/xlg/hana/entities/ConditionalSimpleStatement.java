a8f00d5ffb0c217999303351564fe1fa08d437c4
Danny Gibson <dgibson8@lion.lmu.edu>
2015-04-25 22:35:35 -0700

    a little progress on conditional simple statements

--- src/main/java/edu/lmu/cs/xlg/hana/entities/ConditionalSimpleStatement.java
+++ src/main/java/edu/lmu/cs/xlg/hana/entities/ConditionalSimpleStatement.java
@@ -7,24 +7,54 @@ package edu.lmu.cs.xlg.hana.entities;
 public class ConditionalSimpleStatement extends Statement {
 
   private Statement simpleStatement;
-  private Expression conditionalExpression;
+  private Expression condition;
+  private String control;
 
-  public ConditionalSimpleStatement(Statement simpleStatement, Expression conditionalExpression) {
+  public ConditionalSimpleStatement(Statement simpleStatement, Expression condition, String control) {
     this.simpleStatement = simpleStatement;
-    this.conditionalExpression = conditionalExpression;
+    this.condition = condition;
+    this.control = control;
   }
 
   public Statement getSimpleStatement() {
     return simpleStatement;
   }
 
-  public Expression getConditionalExpressio() {
-    return conditionalExpression;
+  public Expression getConditionalExpression() {
+    return condition;
+  }
+
+  public String getControl() {
+    return control;
   }
 
   @Override
   public void analyze(AnalysisContext context) {
+    if (control.equals("if")) {
+      condition.analyze(context);
+      condition.assertBoolean("if_condition_not_boolean", context);
+      simpleStatement.analyze();
+    } else if (control.equals("while")) {
+
+    } else if (control.equals("unless")) {
+
+    } else { // control equals until
+
+    }
+
     
   }
 
+  @Override
+  public Statement optimize() {
+    if ((control.equals("while") || control.equals("if")) && condition.isFalse()) {
+      // "while false"" or "if false" are both no-ops
+      return null;
+    }
+    if ((control.equals("unless") || control.equals("until")) && condition.isTrue()) {
+      // "unless true" or "until true" are both no-ops
+      return null
+    }
+    return this;
+  }
 }
